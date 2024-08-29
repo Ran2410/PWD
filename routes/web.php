bd8da065fec9bb7a6fbee5ff744afbc2d6e7d2cf
Rann2410 <ran241027@gmail.com>
2024-08-29 17:13:20 +0700

    bacok

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });
 
@@ -21,3 +21,6 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
